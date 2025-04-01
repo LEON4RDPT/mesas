6344265f491d8a6d1803ec73a29c3d6294099d0f
leoo <avramadriana3@gmail.com>
2025-04-01 09:36:08 +0100

    Mesas

---
+++ src/app/interfaces/mesas.ts
@@ -0,0 +1,8 @@
+export interface Mesas {
+    id: number,
+    limiteTempoMin: number,
+    localX: number,
+    localY: number,
+    CapacidadeUsers: number,
+    ativo: boolean
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { UtilizadoresComponent } from "../../components/utilizadores/utilizadores.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [UtilizadoresComponent],
+  imports: [UtilizadoresComponent, CommonModule, ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  selectedPage = 3; 
+  /*
+  0 = Mesas
+  1 = Calendario
+  2 = Estatisticas
+  3 = Utilizadores
+  */
+
 
 }
